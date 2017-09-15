6fdd356aaa5a1ae41f216c07b2c53abad0e6ad40
Wesley Choi <weschoi.creative@gmail.com>
2017-09-15 16:46:48 -0700

    change server to include http

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-var express = require('express');
+// var express = require('express');
+// var app = express();
 
-var app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.static(__dirname + '/../dist'));
+
+// app.listen(PORT, function() {
+//   console.log('listening on port 3000!');
+// });
+
+
+
+
+
+const http = require('http');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static('dist'));
+app.set('port', PORT);
 
-app.listen(PORT, function() {
-  console.log('listening on port 3000!');
+const server = http.createServer(app);
+
+server.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}...`)
 });
