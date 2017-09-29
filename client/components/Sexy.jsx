688c92096e7a8a3d5d1234a743d5f7875dd6f706
Wesley Choi <weschoi.creative@gmail.com>
2017-09-28 23:30:16 -0700

    add iphone

--- client/components/Sexy.jsx
+++ client/components/Sexy.jsx
@@ -54,89 +54,99 @@ class Sexy extends React.Component {
       <div className="col-md-6 sexy">
 
         <div>
-          <div className="view">
-            <div className="view-header">
-              <div className="signal">
-                <div>
-                  <div className="signal-circle-1"></div>
-                  <div className="signal-circle-1"></div>
-                  <div className="signal-circle-1"></div>
-                  <div className="signal-circle-2"></div>
-                  <div className="signal-circle-2"></div>
+
+          <div className="iphone-section-1">
+            <div className="long-rectangle"></div>
+            <div className="short-rectangle"></div>
+          </div>
+          <div className="iphone-section-2">
+            <div className="view">
+              <div className="view-header">
+                <div className="signal">
+                  <div>
+                    <div className="signal-circle-1"></div>
+                    <div className="signal-circle-1"></div>
+                    <div className="signal-circle-1"></div>
+                    <div className="signal-circle-2"></div>
+                    <div className="signal-circle-2"></div>
+                  </div>
+                </div>
+                <div className="time">
+                  {this.state.time}
+                </div>
+                <div className="battery">
+                  <div className="battery-percent">42%</div>
+                  <div className="battery-icon">
+                  </div>
                 </div>
               </div>
-              <div className="time">
-                {this.state.time}
+              <div className="result">
+                {this.state.result}
               </div>
-              <div className="battery">
-                <div className="battery-percent">42%</div>
-                <div className="battery-icon">
+              <div className="view-footer">>
+                <div className="x">
+                  <div>
+                    x
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="result">
-              {this.state.result}
-            </div>
-            <div className="view-footer">>
-              <div className="x">
-                <div>
+                <div className="previous-record">
                   x
                 </div>
               </div>
-              <div className="previous-record">
-                x
-              </div>
+
             </div>
 
-          </div>
+            <div className="history">
+              <span id="history-text">{this.state.record}</span>
+            </div>
 
-          <div className="history">
-            <span id="history-text">{this.state.record}</span>
-          </div>
+            <div className='button-group'>
 
-          <div className='button-group'>
-
-            <div>
-              <div onClick={() => {this.handleClear() }}>
-                <div>
-                  C
+              <div>
+                <div onClick={() => {this.handleClear() }}>
+                  <div>
+                    C
+                  </div>
                 </div>
+                <div className="operator">+/-</div>
+                <div className="operator">%</div>
+                <div onClick={() => {this.handleClick('/') }} className="operator">/</div>
               </div>
-              <div>+/-</div>
-              <div>%</div>
-              <div onClick={() => {this.handleClick('/') }}>/</div>
-            </div>
 
-            <div>
-              <div onClick={() => {this.handleClick('7') }}>7</div>
-              <div onClick={() => {this.handleClick('8') }}>8</div>
-              <div onClick={() => {this.handleClick('9') }}>9</div>
-              <div onClick={() => {this.handleClick('*') }}>x</div>
-            </div>
+              <div>
+                <div onClick={() => {this.handleClick('7') }}>7</div>
+                <div onClick={() => {this.handleClick('8') }}>8</div>
+                <div onClick={() => {this.handleClick('9') }}>9</div>
+                <div onClick={() => {this.handleClick('*') }} className="operator">x</div>
+              </div>
 
-            <div>
-              <div onClick={() => {this.handleClick('4') }}>4</div>
-              <div onClick={() => {this.handleClick('5') }}>5</div>
-              <div onClick={() => {this.handleClick('6') }}>6</div>
-              <div onClick={() => {this.handleClick('-') }}>-</div>
-            </div>
+              <div>
+                <div onClick={() => {this.handleClick('4') }}>4</div>
+                <div onClick={() => {this.handleClick('5') }}>5</div>
+                <div onClick={() => {this.handleClick('6') }}>6</div>
+                <div onClick={() => {this.handleClick('-') }} className="operator">-</div>
+              </div>
 
-            <div>
-              <div onClick={() => {this.handleClick('1') }}>1</div>
-              <div onClick={() => {this.handleClick('2') }}>2</div>
-              <div onClick={() => {this.handleClick('3') }}>3</div>
-              <div onClick={() => {this.handleClick('+') }}>+</div>
-            </div>
+              <div>
+                <div onClick={() => {this.handleClick('1') }}>1</div>
+                <div onClick={() => {this.handleClick('2') }}>2</div>
+                <div onClick={() => {this.handleClick('3') }}>3</div>
+                <div onClick={() => {this.handleClick('+') }} className="operator">+</div>
+              </div>
 
-            <div>
-              <div onClick={() => {this.handleClick('.') }}>.</div>
-              <div onClick={() => {this.handleClick('0') }}>0</div>
-              <div className="sexy-button-equal" onClick={() => {this.handleEnter()}}>=</div>
-            </div>
+              <div>
+                <div onClick={() => {this.handleClick('.') }}>.</div>
+                <div onClick={() => {this.handleClick('0') }}>0</div>
+                <div className="sexy-button-equal" onClick={() => {this.handleEnter()}}>=</div>
+              </div>
 
+            </div>
+          </div>
+          <div className="iphone-section-3">
           </div>
 
         </div>
+
       </div>
     )
   }
