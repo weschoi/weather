5b8e3eadc687d8853b1f9bbf724b7159c41372ce
Wesley Choi <weschoi.creative@gmail.com>
2017-11-20 22:45:40 -0800

    change API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import $ from 'jquery';
-import dummy from './dummy';
-import kelvinToFahrenheit from 'kelvin-to-fahrenheit';
 
 
 export default class App extends React.Component {
@@ -10,97 +8,64 @@ export default class App extends React.Component {
     this.state = {
       chicago: '',
       miami: '',
-      nyc: '',
+      'new york city': '',
       seattle: '',
-      sf: '',
+      'san francisco': '',
       cities: ['miami', 'new york city', 'san francisco', 'chicago', 'seattle'],
-      selectedCity: 'sf'
+      selectedCity: 'san francisco'
     }
   }
 
-  convertAndRound(num){
-    return Math.round(kelvinToFahrenheit(num))
-  }
-
   componentWillMount() {
-    // $.get('https://api.apixu.com/v1/current.json?key=165181a757d242c882d50843172111&q=chicago').done(data => console.log(data));
-    // $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=chicago&days=5').done(data => console.log(data));
-
-           // https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=&days=3
-
-
-    $.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?id=4887398&appid=471988b0c33796818cc9ca5568400bbd').done(data => this.setState({chicago: data}));
-    $.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?id=4542692&appid=471988b0c33796818cc9ca5568400bbd').done(data => this.setState({miami: data}));
-    $.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?id=4900961&appid=471988b0c33796818cc9ca5568400bbd').done(data => this.setState({nyc: data}));
-    $.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?id=5809844&appid=471988b0c33796818cc9ca5568400bbd').done(data => this.setState({seattle: data}));
-    $.get('https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?id=5391959&appid=471988b0c33796818cc9ca5568400bbd').done(data => this.setState({sf: data}));
-
-    this.setForecast(4887398, 'chicago');
-    this.setForecast(4542692, 'miami');
-    this.setForecast(4900961, 'nyc');
-    this.setForecast(5809844, 'seattle');
-    this.setForecast(5391959, 'sf');
+    $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=chicago&days=6').done(data => this.setState({chicago: data}));
+    $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=miami&days=6').done(data => this.setState({miami: data}));
+    $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=new york city&days=6').done(data => this.setState({'new york city': data}));
+    $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=seattle&days=6').done(data => this.setState({seattle: data}));
+    $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=94121&days=6').done(data => this.setState({'san francisco': data}));
   }
 
-  setForecast(id, city) {
+  getDays() {
+    let date = new Date().getDay();
+    let days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday'];
 
+    return days.slice(date+1, date+6)
+  }
 
-    $.get(`http://api.openweathermap.org/data/2.5/forecast?id=${id}&appid=471988b0c33796818cc9ca5568400bbd`)
-      .done(data => {
-        let date = new Date().getDay();
-        let result = [];
-        let days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday'];
+  getForecast() {
 
-        for (let i = date + 1; i < date + 6; i++) {
-          result.push(days[i])
-        }
+    let state = this.state;
+    let selectedCity = state.selectedCity;
 
-        let list = data.list;
-
-        let cityObj = Object.assign({}, this.state[city])
-        cityObj.forecast = [
-          {day: result[0], temp: this.convertAndRound(list[2].main.temp)}, 
-          {day: result[1], temp: this.convertAndRound(list[10].main.temp)}, 
-          {day: result[2], temp: this.convertAndRound(list[18].main.temp)}, 
-          {day: result[3], temp: this.convertAndRound(list[26].main.temp)}, 
-          {day: result[4], temp: this.convertAndRound(list[34].main.temp)}
-        ];
-
-        let newObj = function() {
-          var obj = {};
-          obj[city] = cityObj;
-          return obj;
-        }
-
-        this.setState(newObj);
-      })
-
+    return state[selectedCity] ? this.getDays().map((day, index) =>
+      <div key={index}>
+        <h1>{day}</h1>
+        <h1>{state[selectedCity].forecast.forecastday[index+1].day.avgtemp_f}</h1>
+      </div>
+    ) : ''
+    
   }
 
   returnCities() {
-    return this.state.cities.map((city, index) => <h1 key={index}>{city}</h1>)
+    return this.state.cities.map((city, index) => <h1 key={index} onClick={() => this.switchCity(city)}>{city}</h1>)
   }
 
-  returnForecast(city) {
-
-
-
-    // return result.map((day, index) => <div><h1 key={index}>{day}</h1><h1>{this.state[city].forecast[0] ? this.state[city].forecast[index] : 'hi'}</h1></div>)
+  switchCity(city) {
+    this.setState({selectedCity: city})
   }
 
   render() {
-        // {this.returnForecast(this.state.selectedCity)}
-
-    let s = this.state;
+    let state = this.state;
+    let selectedCity = state.selectedCity;
 
     return (
       <div>
         {this.returnCities()}
-        <h1>{s[s.selectedCity].name}</h1>
-        <h1>{s[s.selectedCity].main ? this.convertAndRound(s[s.selectedCity].main.temp) : '90'}</h1>
-        <h1>{s[s.selectedCity].weather ? s[s.selectedCity].weather[0].description : 'd'}</h1>
-        {this.returnForecast(s.selectedCity)}
+        <h1>{state.selectedCity}</h1>
+        <h1>{state[selectedCity] ? state[selectedCity].current.temp_f : ''}</h1>
+        <h1>{state[selectedCity] ? state[selectedCity].current.condition.text : ''}</h1>
+        {this.getForecast()}
       </div>
     )
   }
 }
+
