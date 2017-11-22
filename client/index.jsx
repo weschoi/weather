3b60900a07618d52a5aedffcae9c5e26d802bf55
Wesley Choi <weschoi.creative@gmail.com>
2017-11-21 22:25:52 -0800

    implement time

--- client/index.jsx
+++ client/index.jsx
@@ -12,11 +12,14 @@ class App extends React.Component {
       seattle: '',
       'san francisco': '',
       cities: ['miami', 'new york city', 'san francisco', 'chicago', 'seattle'],
-      selectedCity: 'san francisco'
+      selectedCity: 'san francisco',
+      time: ''
     }
   }
 
   componentWillMount() {
+    this.getTime();
+
     $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=chicago&days=6').done(data => this.setState({chicago: data}));
     $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=miami&days=6').done(data => this.setState({miami: data}));
     $.get('https://api.apixu.com/v1/forecast.json?key=165181a757d242c882d50843172111&q=new york city&days=6').done(data => this.setState({'new york city': data}));
@@ -39,14 +42,24 @@ class App extends React.Component {
     return state[selectedCity] ? this.getDays().map((day, index) =>
       <div key={index}>
         <h1>{day}</h1>
-        <h1>{state[selectedCity].forecast.forecastday[index+1].day.avgtemp_f}</h1>
+        <h1>{Math.round(state[selectedCity].forecast.forecastday[index+1].day.avgtemp_f)}</h1>
       </div>
     ) : ''
     
   }
 
+  getTime() {
+    this.setState({time: new Date().getHours() });
+}
+
   returnCities() {
-    return this.state.cities.map((city, index) => <div key={index} onClick={() => this.switchCity(city)}>{city}</div>)
+    return this.state.cities.map((city, index) => {
+      if (city === this.state.selectedCity) {
+        return <div key={index} style={{opacity: '1'}} onClick={() => this.switchCity(city)}>{city}</div>
+      } else {
+        return <div key={index} style={{opacity: '0.5'}} onClick={() => this.switchCity(city)}>{city}</div>
+      }
+    })
   }
 
   switchCity(city) {
@@ -63,7 +76,7 @@ class App extends React.Component {
           <div className="col-sm-6 col-10">
             <div>{this.returnCities()}</div>
             <div>
-              <h1>{state[selectedCity] ? state[selectedCity].current.temp_f : ''}<sup>o</sup></h1>
+              <h1>{state[selectedCity] ? Math.round(state[selectedCity].current.temp_f) : ''}&#176;</h1>
               <h1>{state[selectedCity] ? state[selectedCity].current.condition.text : ''}</h1>
             </div>
             <div>
