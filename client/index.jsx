a347005d06898e6614efb43348925bb6f81e628f
Wesley Choi <weschoi.creative@gmail.com>
2019-02-15 13:50:57 -0800

    change cursor to pointer

--- client/index.jsx
+++ client/index.jsx
@@ -200,17 +200,11 @@ class App extends React.Component {
   switchCity(city) {
     this.setState({selectedCity: city});
 
-    if (city === 'new york city') {
-      this.getTime(3);
-    } else if (city === 'tokyo') {
-      this.getTime(17);
-    }else if (city === 'london') {
-      this.getTime(8);
-    } else if (city === 'san francisco') {
-      this.getTime(0);
-    } else if (city === 'moscow') {
-      this.getTime(11);
-    }
+    if (city === 'new york city') this.getTime(3);
+    if (city === 'tokyo') this.getTime(17);
+    if (city === 'london') this.getTime(8);
+    if (city === 'san francisco') this.getTime(0);
+    if (city === 'moscow') this.getTime(11);
   }
 
   getSunOrMoonPosition(hour) {
@@ -257,23 +251,17 @@ class App extends React.Component {
   render() {
     let state = this.state;
     let selectedCity = state.selectedCity;
-    var style = {};
-    let that = this;
+    let style = {
+      backgroundRepeat: 'no-repeat, no-repeat',
+      backgroundSize: '15%, 100%'
+    };
 
     if (state.time >= 7 && state.time <= 18) {
-      style = {
-        backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 50 50'><filter id='f1'><feGaussianBlur in='SourceGraphic' stdDeviation='1' /></filter><circle cx='25' cy='25' r='21' fill='${this.state.sun}' filter='url(#f1)'/></svg>"), linear-gradient(180deg,${this.state.gradient1} 0%, ${this.state.gradient2} 50%, ${this.state.gradient2} 100%)`,
-        backgroundRepeat: 'no-repeat, no-repeat',
-        backgroundSize: '15%, 100%',
-        backgroundPosition: `15% ${this.state.sunOrMoonPosition}, top`
-      }
+      style.backgroundImage = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='0 0 50 50'><filter id='f1'><feGaussianBlur in='SourceGraphic' stdDeviation='1' /></filter><circle cx='25' cy='25' r='21' fill='${this.state.sun}' filter='url(#f1)'/></svg>"), linear-gradient(180deg,${this.state.gradient1} 0%, ${this.state.gradient2} 50%, ${this.state.gradient2} 100%)`;
+      style.backgroundPosition = `15% ${this.state.sunOrMoonPosition}, top`;
     } else {
-      style = {
-        backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='141.7 -5 57 57'><filter id='f1'><feGaussianBlur in='SourceGraphic' stdDeviation='1' /></filter><path d='M170.6 0.5c-11.2-2.2-22.4 3.4-27.4 13.6 3.3-4.4 8.6-7.3 14.6-7.3 7.9 0 14.6 5 17.1 12 0.1 0.2 0.1 0.4 0.2 0.5 0.1 0.3 0.2 0.5 0.2 0.8 0.1 0.3 0.1 0.5 0.2 0.8 0.1 0.2 0.1 0.5 0.2 0.7 0.1 0.3 0.1 0.7 0.2 1.1 0 0.2 0.1 0.4 0.1 0.6 0.1 0.6 0.1 1.1 0.1 1.7 0 0.5 0 1.1-0.1 1.6l0 0c-0.8 9.3-8.6 16.6-18.2 16.6 -6 0-11.2-2.9-14.6-7.3 4.9 10.2 16.2 15.8 27.4 13.6 11.7-2.3 20.1-12.6 20.1-24.5C190.7 13.1 182.3 2.8 170.6 0.5z' filter='url(#f1)' fill='#FFF'/></svg>"), linear-gradient(180deg,${this.state.gradient1} 0%, ${this.state.gradient2} 50%, ${this.state.gradient2} 100%)`,
-        backgroundRepeat: 'no-repeat, no-repeat',
-        backgroundSize: '15%, 100%',
-        backgroundPosition: `82% ${this.state.sunOrMoonPosition}, top`
-      } 
+      style.backgroundImage = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='50' height='50' viewBox='141.7 -5 57 57'><filter id='f1'><feGaussianBlur in='SourceGraphic' stdDeviation='1' /></filter><path d='M170.6 0.5c-11.2-2.2-22.4 3.4-27.4 13.6 3.3-4.4 8.6-7.3 14.6-7.3 7.9 0 14.6 5 17.1 12 0.1 0.2 0.1 0.4 0.2 0.5 0.1 0.3 0.2 0.5 0.2 0.8 0.1 0.3 0.1 0.5 0.2 0.8 0.1 0.2 0.1 0.5 0.2 0.7 0.1 0.3 0.1 0.7 0.2 1.1 0 0.2 0.1 0.4 0.1 0.6 0.1 0.6 0.1 1.1 0.1 1.7 0 0.5 0 1.1-0.1 1.6l0 0c-0.8 9.3-8.6 16.6-18.2 16.6 -6 0-11.2-2.9-14.6-7.3 4.9 10.2 16.2 15.8 27.4 13.6 11.7-2.3 20.1-12.6 20.1-24.5C190.7 13.1 182.3 2.8 170.6 0.5z' filter='url(#f1)' fill='#FFF'/></svg>"), linear-gradient(180deg,${this.state.gradient1} 0%, ${this.state.gradient2} 50%, ${this.state.gradient2} 100%)`;
+      style.backgroundPosition = `82% ${this.state.sunOrMoonPosition}, top`;
     }
 
     return (
